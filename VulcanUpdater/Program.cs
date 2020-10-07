bb6d9b738c86faa65032509ecd46d8b9883299dd
Adrian Ciołek <qwizi95@protonmail.com>
2020-10-07 21:13:04 +0200

    Dodano logowanie oraz od teraz konsola sie chowa

--- VulcanUpdater/Program.cs
+++ VulcanUpdater/Program.cs
@@ -10,96 +10,82 @@ using System.Diagnostics;
 using Octokit;
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.InteropServices;
+using Serilog;
+using Serilog.Events;
 
 namespace VulcanUpdater
 {
     class Program
     {
+        private static GitHubClient GhClient { get; set; }
+        private static HttpClient HClient { get; set; }
+        private static string CurrentDir { get; set; }
+        
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Vulcan Updater");
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("config.json");
-            
-            var config = builder.Build();
-            var adress = config.GetSection("Adress").Value;
-            Console.WriteLine(adress);
-            var uri = new Uri(adress +"updater");
-            var socket = new SocketIO(uri);
-            
-            socket.OnConnected += Socket_OnConnected;
-            socket.OnDisconnected += Socket_onDisconnected;
-            
-            socket.On("download_new_version", async response =>
+            var handle = GetConsoleWindow();
+            // Chowamy okno konsoli
+            ShowWindow(handle, SW_HIDE);
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(Directory.GetCurrentDirectory() + "/logs/LogFile.txt")
+                .CreateLogger();
+            try
             {
-                Console.WriteLine(response);
-                JObject release = response.GetValue(0).Value<JObject>("release");
-                var assets = release.GetValue("assets");
+                Log.Information("Vulcan Updater 1.0.3");
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("config.json");
+            
+                var config = builder.Build();
+                var adress = config.GetSection("Adress").Value;
+                Console.WriteLine(adress);
+                var uri = new Uri(adress +"updater");
+                var socket = new SocketIO(uri);
+            
+                CurrentDir = Directory.GetCurrentDirectory();
+                string clientFilename = $"{CurrentDir}/client/VulcanClient2.exe";
+            
+                Process process = new Process();
+                process.StartInfo.FileName = clientFilename;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+            
+            
+                GhClient = new GitHubClient(ProductHeaderValue.Parse("Vulcan"));
+                HClient = new HttpClient();
+                HClient.DefaultRequestHeaders.Add("User-Agent", "Vulcan");
+            
+                socket.OnConnected += Socket_OnConnected;
+                socket.OnDisconnected += Socket_onDisconnected;
                 
-                foreach (JObject asset in assets)
+                socket.On("download_new_version", async response =>
                 {
-                    Console.WriteLine(asset);
-                    var assetName = asset.GetValue("name").ToString();
-                    if (assetName == "binaries.zip")
+                    Console.WriteLine(response);
+                    JObject release = response.GetValue(0).Value<JObject>("release");
+                    var assets = release.GetValue("assets");
+                    
+                    foreach (JObject asset in assets)
                     {
-                        string zipName = asset.GetValue("name").ToString();
-                        string BrowserDownloadUrl = asset.GetValue("browser_download_url").ToString();
-                        using (HttpClient client = new HttpClient())
+                        Console.WriteLine(asset);
+                        var assetName = asset.GetValue("name").ToString();
+                        if (assetName == "binaries.zip")
                         {
-                            client.DefaultRequestHeaders.Add("User-Agent", "Vulcan");
-                            using (HttpResponseMessage res = await client.GetAsync(BrowserDownloadUrl,
-                                HttpCompletionOption.ResponseHeadersRead))
-                            {
-                                res.EnsureSuccessStatusCode();
-                                var currentDir = Directory.GetCurrentDirectory();
-                                string filePath = $"{currentDir}/{zipName}";
-                                var extractPath = $"{currentDir}/client/";
-                                
-                                using (Stream stream = await res.Content.ReadAsStreamAsync())
-                                {
-                                    using (Stream streamToWrite = File.Open(filePath, System.IO.FileMode.Create))
-                                    {
-                                        await stream.CopyToAsync(streamToWrite);
-                                    }
-                                }
-
-                                await Task.Run(() => ZipFile.ExtractToDirectory(filePath, extractPath, true));
-                                await Task.Run(() => File.Delete(filePath));
-                            }
-                        }
-                    }
-                }
-            });
-            
-            await socket.ConnectAsync();
-            
-            var currentDir = Directory.GetCurrentDirectory();
-            string clientFilename = $"{currentDir}/client/VulcanClient2.exe";
-            Process process = new Process();
-            process.StartInfo.FileName = clientFilename;
-            process.StartInfo.CreateNoWindow = false;
-            process.StartInfo.UseShellExecute = true;
-            
-            var ghClient = new GitHubClient(ProductHeaderValue.Parse("Vulcan"));
-            
-            if (File.Exists(clientFilename))
-            {
-                process.Start();
-                string currentClientVersion = process.MainModule.FileVersionInfo.ProductVersion;
-                var releases = await ghClient.Repository.Release.GetAll("Qwizi", "Vulcan-Client-2");
-                var latest = releases[0];
-                if (latest.TagName != currentClientVersion)
-                {
-                    Console.WriteLine("Pobieramy najnowsza wersje Vulcana");
-                    process.Kill();
-                    var assets = latest.Assets;
-                    foreach (var asset in assets) 
-                    {
-                        if (asset.Name == "binaries.zip")
-                        {
-                            string zipName = asset.Name;
-                            string BrowserDownloadUrl = asset.BrowserDownloadUrl;
+                            string zipName = asset.GetValue("name").ToString();
+                            string BrowserDownloadUrl = asset.GetValue("browser_download_url").ToString();
                             using (HttpClient client = new HttpClient())
                             {
                                 client.DefaultRequestHeaders.Add("User-Agent", "Vulcan");
@@ -107,9 +93,10 @@ namespace VulcanUpdater
                                     HttpCompletionOption.ResponseHeadersRead))
                                 {
                                     res.EnsureSuccessStatusCode();
+                                    var currentDir = Directory.GetCurrentDirectory();
                                     string filePath = $"{currentDir}/{zipName}";
                                     var extractPath = $"{currentDir}/client/";
-                                
+                                    
                                     using (Stream stream = await res.Content.ReadAsStreamAsync())
                                     {
                                         using (Stream streamToWrite = File.Open(filePath, System.IO.FileMode.Create))
@@ -120,68 +107,155 @@ namespace VulcanUpdater
 
                                     await Task.Run(() => ZipFile.ExtractToDirectory(filePath, extractPath, true));
                                     await Task.Run(() => File.Delete(filePath));
-                                    process.Start();
                                 }
                             }
                         }
                     }
-                }
-            }
-            else
-            {
-                Console.WriteLine("Pobieramy najnowsza wersje Vulcana");
+                });
                 
-                var releases = await ghClient.Repository.Release.GetAll("Qwizi", "Vulcan-Client-2");
-                var latest = releases[0];
-                var assets = latest.Assets;
-                
-                foreach (var asset in assets) 
+                await socket.ConnectAsync();
+
+                if (File.Exists(clientFilename))
                 {
-                    if (asset.Name == "binaries.zip")
-                    {
-                        string zipName = asset.Name;
-                        string BrowserDownloadUrl = asset.BrowserDownloadUrl;
-                        using (HttpClient client = new HttpClient())
-                        {
-                            client.DefaultRequestHeaders.Add("User-Agent", "Vulcan");
-                            using (HttpResponseMessage res = await client.GetAsync(BrowserDownloadUrl,
-                                HttpCompletionOption.ResponseHeadersRead))
-                            {
-                                res.EnsureSuccessStatusCode();
-                                string filePath = $"{currentDir}/{zipName}";
-                                var extractPath = $"{currentDir}/client/";
-                                
-                                using (Stream stream = await res.Content.ReadAsStreamAsync())
-                                {
-                                    using (Stream streamToWrite = File.Open(filePath, System.IO.FileMode.Create))
-                                    {
-                                        await stream.CopyToAsync(streamToWrite);
-                                    }
-                                }
-
-                                await Task.Run(() => ZipFile.ExtractToDirectory(filePath, extractPath, true));
-                                await Task.Run(() => File.Delete(filePath));
-                                process.Start();
-                            }
-                        }
-                    }
+                    await DownloadNewVersionOrRunProcess(process, clientFilename);
                 }
-            }
-
-            await process.WaitForExitAsync();
+                else
+                {
+                    await DownloadVLatestVulcanVersion(process);
+                }
+                
+                await process.WaitForExitAsync();
             
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Wystapil problem z updaterem");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static void Socket_OnConnected(object sender, EventArgs e)
         {
             var socket = sender as SocketIO;
-            Console.WriteLine("Updater polaczony " + socket.Id);
+            Log.Information("Updater polaczony " + socket.Id);
         }
 
         private static void Socket_onDisconnected(object sender, string e)
         {
-            Console.WriteLine("Updater rozlaczony");
+            Log.Information("Updater rozlaczony");
+        }
+
+        private static async Task DownloadVLatestVulcanVersion(Process process)
+        {
+            Log.Information("Pobieramy najnowsza wersje Vulcana");
+                
+            var releases = await GhClient.Repository.Release.GetAll("Qwizi", "Vulcan-Client-2");
+            var latest = releases[0];
+            var assets = latest.Assets;
+                
+            foreach (var asset in assets) 
+            {
+                if (asset.Name == "binaries.zip")
+                {
+                    string zipName = asset.Name;
+                    string BrowserDownloadUrl = asset.BrowserDownloadUrl;
+
+                    using (HttpResponseMessage res = await HClient.GetAsync(BrowserDownloadUrl,
+                        HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        res.EnsureSuccessStatusCode();
+                        string filePath = $"{CurrentDir}/{zipName}";
+                        var extractPath = $"{CurrentDir}/client/";
+                        Log.Information("Pobieramy klienta");
+                        using (Stream stream = await res.Content.ReadAsStreamAsync())
+                        {
+                            using (Stream streamToWrite = File.Open(filePath, System.IO.FileMode.Create))
+                            {
+                                await stream.CopyToAsync(streamToWrite);
+                            }
+                        }
+
+                        await Task.Run(() =>
+                        {
+                            Log.Information("Rozpakowujemy klienta");
+                            ZipFile.ExtractToDirectory(filePath, extractPath, true);
+                            Log.Information("Klient rozpakowany");
+                        });
+                        await Task.Run(() =>
+                        {
+                            Log.Information("Usuwamy zipa");
+                            File.Delete(filePath);
+                            Log.Information("Zip usuniety");
+                        });
+                        Log.Information("Uruchamiany klienta");
+                        process.Start();
+                        Log.Information("Klient uruchomiony");
+                    }
+                }
+            }
+        }
+
+        private static async Task DownloadNewVersionOrRunProcess(Process process, string clientFilename)
+        {
+            var clientVersion = FileVersionInfo.GetVersionInfo(clientFilename);
+            string currentClientVersion = clientVersion.ProductVersion;
+            var releases = await GhClient.Repository.Release.GetAll("Qwizi", "Vulcan-Client-2");
+            var latest = releases[0];
+            Log.Information("Uruchamiany klienta");
+            process.Start();
+            Log.Information("Klient uruchomiony");
+            if (latest.TagName != currentClientVersion)
+            {
+                Log.Information("Pobieramy najnowsza wersje Vulcana");
+                process.Kill();
+                Log.Debug("Process zamkniety");
+                var assets = latest.Assets;
+                foreach (var asset in assets)
+                {
+                    if (asset.Name == "binaries.zip")
+                    {
+                        string zipName = asset.Name;
+                        string BrowserDownloadUrl = asset.BrowserDownloadUrl;
+                        
+                        using (HttpResponseMessage res = await HClient.GetAsync(BrowserDownloadUrl,
+                            HttpCompletionOption.ResponseHeadersRead))
+                        {
+                            res.EnsureSuccessStatusCode();
+                            string filePath = $"{CurrentDir}/{zipName}";
+                            var extractPath = $"{CurrentDir}/client/";
+                            Log.Information("Pobieramy klienta");
+                            using (Stream stream = await res.Content.ReadAsStreamAsync())
+                            {
+                                using (Stream streamToWrite = File.Open(filePath, System.IO.FileMode.Create))
+                                {
+                                    await stream.CopyToAsync(streamToWrite);
+                                    Log.Information("Klient pobrany");
+                                }
+                            }
+
+                            await Task.Run(() =>
+                            {
+                                Log.Information("Rozpakowujemy klienta");
+                                ZipFile.ExtractToDirectory(filePath, extractPath, true);
+                                Log.Information("Klient rozpakowany");
+                            });
+                            await Task.Run(() =>
+                            {
+                                Log.Information("Usuwamy zipa");
+                                File.Delete(filePath);
+                                Log.Information("Zip usuniety");
+                            });
+                            Log.Information("Uruchamiany klienta");
+                            process.Start();
+                            Log.Information("Klient uruchomiony");
+                        }
+                    }
+                }
+            }
         }
     }
 }
